4beb154532d76099597d996d9e31c9a40dcac814
Marco <macto.14@gmail.com>
2024-01-12 18:41:13 -0300

    módulo dash movido para app

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 import os
 
-from dash_app import app
+import dash_mantine_components as dmc
+from dash import Dash, dcc, html, page_container
 from dotenv import load_dotenv
 from flask import Flask
 from utils.models import login_manager, mongo
@@ -13,9 +14,41 @@ server.config.update(MONGO_URI=os.environ.get("DB_ECONODATA"))
 
 mongo.init_app(server)
 login_manager.init_app(server)
-app.init_app(server)
+
+app = Dash(
+    use_pages=True,
+    suppress_callback_exceptions=True,
+    title="App Econodata",
+    update_title=None,
+    server=server,
+    prevent_initial_callbacks=True,
+)
+
+app.layout = dmc.MantineProvider(
+    [
+        dcc.Store(id="theme-store", storage_type="local"),
+        dcc.Location(id="url", refresh=True),
+        dmc.NotificationsProvider(
+            [
+                html.Div(
+                    dmc.Container(
+                        page_container,
+                        fluid=True,
+                        px=0,
+                    ),
+                    style={"height": "100vh"},
+                )
+            ]
+        ),
+    ],
+    theme={"colorScheme": "light", "primaryColor": "yellow"},
+    id="mantine-main-provider",
+    inherit=True,
+    withGlobalStyles=True,
+    withNormalizeCSS=True,
+)
 
 
 if __name__ == "__main__":
     # app.run(debug=True)
-    server.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    server.run(debug=False, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
