847bb0e08728670e07f5447841f1f2a189e9c2a9
Marco <macto.14@gmail.com>
2024-02-05 21:47:57 -0300

    grafico basico

--- src/pages/user/dashboard.py
+++ src/pages/user/dashboard.py
@@ -1,7 +1,12 @@
+from datetime import datetime
+
 import dash_mantine_components as dmc
+import plotly.graph_objects as go
+import polars as pl
 from components.no_permission import no_permission_layout
-from dash import html, register_page
+from dash import dcc, html, register_page
 from flask_login import current_user
+from utils.models import mongo
 
 register_page(__name__, path="/user/dashboard", title="Dashboard")
 
@@ -9,9 +14,23 @@ register_page(__name__, path="/user/dashboard", title="Dashboard")
 def layout():
     if not current_user.is_authenticated:
         return no_permission_layout
-    else:
-        return html.Div(
-            [
-                html.Div([dmc.Text("Dashboard")], className="content"),
-            ]
+
+    dados = pl.DataFrame(
+        mongo.cx["Econodata"]["Dados"].find(
+            {"c": 1207, "d": {"$gt": datetime(2000, 1, 1)}}, {"_id": 0, "d": 1, "v": 1}
         )
+    )
+    fig = go.Figure(
+        data=[
+            go.Scatter(
+                x=dados.get_column("d"), y=dados.get_column("v"), mode="lines+markers"
+            )
+        ]
+    )
+
+    return html.Div(
+        [
+            dcc.Graph(figure=fig),
+            html.Div([dmc.Text("Dashboard")], className="content"),
+        ]
+    )
